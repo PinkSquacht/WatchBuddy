91ec2951b261a0c6be426620c8608951ba3b651b
Mary Hubert <mhubert314@gmail.com>
2024-05-18 19:49:02 -0700

    adding flask to WatchBuddy

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config():
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
