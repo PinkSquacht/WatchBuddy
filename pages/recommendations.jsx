d59fc98f9991880670eea189cd98ffd20f74e8ff
PinkSquatcht <lloyd.bowar@gmail.com>
2024-06-18 20:36:10 -0700

    Backend is fixed

--- pages/recommendations.jsx
+++ pages/recommendations.jsx
@@ -22,7 +22,7 @@ const Recommendations = () => {
 
       const fetchRecommendations = async () => {
         try {
-          const response = await axios.post('http://127.0.0.1:5000/api/recommendations', {
+          const response = await axios.post('https://chat-w-flask.onrender.com/api/recommendations', {
             movie,
             streaming_services: services ? services.split(',') : [],
             genres: genres ? genres.split(',') : [],
